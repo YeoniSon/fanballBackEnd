3686189eff092c9efea550f299ae5ec8cf77275f
YeoniSon <sjy8211@gmail.com>
2025-10-01 00:45:56 +0900

    feat: 기본 세팅

---
+++ src/main/java/com/example/fanball/FanballApplication.java
@@ -0,0 +1,13 @@
+package com.example.fanball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanballApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fanball/FanballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fanball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
